8b629700daf1f678735d7c452313d967137ac4f0
Clzzi <clzzi1109@naver.com>
2021-10-05 14:21:58 +0900

    Add: yarn berry setting & emotion setting

--- dgsw-kakaobank/src/App.tsx
+++ dgsw-kakaobank/src/App.tsx
@@ -1,10 +1,19 @@
+import { css } from '@emotion/react'
+
+const testStyle = css`
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  width: 100vw;
+  height: 400px;
+`
 
 const App = () => {
   return (
-<div>
-  DGSW KAKAOBANK
-</div>
-  );
+    <div>
+      <div css={testStyle}>hihi</div>
+    </div>
+  )
 }
 
-export default App;
+export default App
