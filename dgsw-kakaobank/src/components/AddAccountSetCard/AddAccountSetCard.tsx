c8ef7a27d4342126fe62442c28e2ad0bfff23fb2
Clzzi <clzzi1109@naver.com>
2021-10-12 14:59:47 +0900

    :lipstick: Add: AddAccountSetCard Component
    카드 추가 컴포넌트 추가

---
+++ dgsw-kakaobank/src/components/AddAccountSetCard/AddAccountSetCard.tsx
@@ -0,0 +1,12 @@
+import { StyledTitle } from "./AddAccountSetCard.style";
+
+const AddAccountSetCard = ():JSX.Element => {
+  return (
+    <StyledTitle>
+      <div>N개의 은행에서</div>
+      <div>N개의 계좌번호를 찾았습니다</div>
+    </StyledTitle>
+  )
+}
+
+export default AddAccountSetCard;
