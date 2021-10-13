8d1d517f8115270fa8cd774d23a612660bc4d7cf
Clzzi <clzzi1109@naver.com>
2021-10-13 21:57:45 +0900

    :construction: Add: Complete Components
    완성 컴포 추가

--- dgsw-kakaobank/src/components/Complete/Complete.style.ts
+++ dgsw-kakaobank/src/components/Complete/Complete.style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { ColorPalette } from 'styles/ColorPalette';
+import { fontPalette } from 'styles/FontPalette';
 
 export const StyledTitle = styled.div`
   display: flex;
@@ -12,5 +13,32 @@ export const StyledTitle = styled.div`
     margin-top: 30px;
     margin-bottom: 70px;
     border-bottom: 1px solid ${ColorPalette.fureBlack};
+  }
+`;
+
+export const StyledButton = styled.div`
+  position: fixed;
+  width: 100vw;
+  bottom: 0px;
+  left: 0px;
+  height: 50px;
+  line-height: 50px;
+  cursor: pointer;
+  background-color: ${ColorPalette.main};
+  color: ${ColorPalette.fureBlack};
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+  font-family: 'AppleEB';
+  font-size: ${fontPalette.font24};
+`;
+
+export const StyledWrapper = styled.div`
+  .line {
+    .property {
+    }
+    .content {
+    }
   }
 `;
--- dgsw-kakaobank/src/components/Complete/Complete.tsx
+++ dgsw-kakaobank/src/components/Complete/Complete.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { UGetPassword } from 'types/common/common.type';
-import { StyledTitle } from './Complete.style';
+import { StyledButton, StyledTitle, StyledWrapper } from './Complete.style';
 import { ReactComponent as Check } from 'assets/icon/check.svg';
 
 interface ICompleteProps {
@@ -14,20 +14,22 @@ interface ICompleteProps {
   remittanceReceiveAccount?: string;
   remittanceMoney?: string;
   remittanceCommission?: string;
+  handleClick: () => void;
 }
 
 const Complete = (Props: ICompleteProps): JSX.Element => {
   const {
+    title,
     depositAccount,
     bringMoney,
     bringAccount,
     type,
-    title,
     depositMoney,
     remittanceCommission,
     remittanceGiveAccount,
     remittanceMoney,
     remittanceReceiveAccount,
+    handleClick,
   } = Props;
 
   return (
@@ -36,6 +38,64 @@ const Complete = (Props: ICompleteProps): JSX.Element => {
         <Check />
         <div className="title">{title}</div>
       </StyledTitle>
+      <StyledWrapper>
+        {remittanceGiveAccount && (
+          <div className="line">
+            <span className="property">출금 계좌: </span>
+            <span className="content">{remittanceGiveAccount}</span>
+          </div>
+        )}
+
+        {remittanceReceiveAccount && (
+          <div className="line">
+            <span className="property">받는 계좌: </span>
+            <span className="content">{remittanceReceiveAccount}</span>
+          </div>
+        )}
+
+        {remittanceMoney && (
+          <div className="line">
+            <span className="property">보낸 금액: </span>
+            <span className="content">{remittanceMoney} 원</span>
+          </div>
+        )}
+
+        {remittanceCommission && (
+          <div className="line">
+            <span className="property">수수료: </span>
+            <span className="content">{remittanceCommission} 원</span>
+          </div>
+        )}
+
+        {depositAccount && (
+          <div className="line">
+            <span className="property">입금 계좌: </span>
+            <span className="content">{depositAccount}</span>
+          </div>
+        )}
+
+        {depositMoney && (
+          <div className="line">
+            <span className="property">입금한 금액: </span>
+            <span className="content">{depositMoney} 원</span>
+          </div>
+        )}
+
+        {bringAccount && (
+          <div className="line">
+            <span className="property">가져온 계좌: </span>
+            <span className="content">{bringAccount}</span>
+          </div>
+        )}
+
+        {bringMoney && (
+          <div className="line">
+            <span className="property">가져온 금액: </span>
+            <span className="content">{bringMoney} 원</span>
+          </div>
+        )}
+      </StyledWrapper>
+      <StyledButton onClick={handleClick}>완료</StyledButton>
     </>
   );
 };
