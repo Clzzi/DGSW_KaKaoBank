5efa30b57809b13f208b6128616fa23c95b9aec2
Clzzi <clzzi1109@naver.com>
2021-10-13 21:34:41 +0900

    :construction: Add: Complete Components
    완료 컴포넌트 추가

--- dgsw-kakaobank/src/components/Complete/Complete.style.ts
+++ dgsw-kakaobank/src/components/Complete/Complete.style.ts
@@ -1,5 +1,16 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+import { ColorPalette } from 'styles/ColorPalette';
 
 export const StyledTitle = styled.div`
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 240px;
+  margin-bottom: 70px;
+  .title {
+    margin-top: 30px;
+    margin-bottom: 70px;
+    border-bottom: 1px solid ${ColorPalette.fureBlack};
+  }
 `;
--- dgsw-kakaobank/src/components/Complete/Complete.tsx
+++ dgsw-kakaobank/src/components/Complete/Complete.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import { UGetPassword } from 'types/common/common.type';
+import { StyledTitle } from './Complete.style';
+import { ReactComponent as Check } from 'assets/icon/check.svg';
 
 interface ICompleteProps {
   title: string;
@@ -13,8 +16,28 @@ interface ICompleteProps {
   remittanceCommission?: string;
 }
 
-const Complete = (): JSX.Element => {
-  return <div>test</div>;
+const Complete = (Props: ICompleteProps): JSX.Element => {
+  const {
+    depositAccount,
+    bringMoney,
+    bringAccount,
+    type,
+    title,
+    depositMoney,
+    remittanceCommission,
+    remittanceGiveAccount,
+    remittanceMoney,
+    remittanceReceiveAccount,
+  } = Props;
+
+  return (
+    <>
+      <StyledTitle>
+        <Check />
+        <div className="title">{title}</div>
+      </StyledTitle>
+    </>
+  );
 };
 
 export default Complete;
