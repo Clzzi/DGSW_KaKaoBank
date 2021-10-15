a932221d9b91db3b17dc7c287339bbc4fadc3f76
Clzzi <clzzi1109@naver.com>
2021-10-15 18:03:38 +0900

    :sparkles: Add: Profile
    프로필 페이지 와성

--- dgsw-kakaobank/src/components/Profile/Profile.tsx
+++ dgsw-kakaobank/src/components/Profile/Profile.tsx
@@ -1,6 +1,11 @@
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from 'store/user';
+import makeBirth from 'util/makeBirth';
+import makePhoneNumber from 'util/makePhoneNumber';
 import { StyledContent, StyledContentWrap, StyledTitle } from './Profile.style';
 
 const Profile = (): JSX.Element => {
+  const userInfo = useRecoilValue(userInfoState);
   return (
     <div>
       <StyledTitle>
@@ -10,19 +15,23 @@ const Profile = (): JSX.Element => {
       <StyledContentWrap>
         <StyledContent>
           <div className="property">이름: </div>
-          <div className="content">손민재</div>
+          <div className="content">{userInfo.name && userInfo.name}</div>
         </StyledContent>
         <StyledContent>
           <div className="property">아이디: </div>
-          <div className="content">jce1407</div>
+          <div className="content">{userInfo.id && userInfo.id}</div>
         </StyledContent>
         <StyledContent>
           <div className="property">전화번호: </div>
-          <div className="content">010-9088-2512</div>
+          <div className="content">
+            {userInfo.phone && makePhoneNumber(userInfo.phone)}
+          </div>
         </StyledContent>
         <StyledContent>
           <div className="property">생년월일: </div>
-          <div className="content">041109-3</div>
+          <div className="content">
+            {userInfo.birth && makeBirth(userInfo.birth)}
+          </div>
         </StyledContent>
       </StyledContentWrap>
     </div>
